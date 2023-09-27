bfbb612fed98237769e98282e143e2bccf6acd6a
Sladrus <ufimkinyasha@gmail.com>
2023-09-27 21:48:13 +0300

    fix

--- middleware/jwtMiddleware.js
+++ middleware/jwtMiddleware.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
 const { UserModel } = require('../models/userModel');
+require('dotenv').config();
 
 const jwtMiddleware = async (socket, next) => {
   const { token } = socket.handshake.auth;
   if (token) {
     try {
-      const decoded = jwt.verify(token, 'secret');
+      const decoded = jwt.verify(token, process.env.SECRET);
       const user = await UserModel.findOne({ email: decoded.email });
       socket.emit('user', { user, token });
     } catch (error) {
