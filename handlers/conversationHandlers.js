45505219dced9908863de8a0416a60968aea1b41
Sladrus <ufimkinyasha@gmail.com>
2023-10-01 22:45:07 +0300

    fix

--- handlers/conversationHandlers.js
+++ handlers/conversationHandlers.js
@@ -1427,7 +1427,7 @@ module.exports = (io, socket) => {
 
   const sendMessage = async ({ id, text, type, user }) => {
     try {
-      return;
+      // return;
       const conversation = await ConversationModel.findOne({
         _id: id,
       });
